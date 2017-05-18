13ddb55ac62406dfb71e2e1e7d38cdbec6ce7c2c
Keegan Jebb <ksjebb@gmail.com>
2017-05-18 12:34:27 -0400

    Finished If Tutorial. Have var.c as header in main.c

--- C-Intro/C-Intro/main.c
+++ C-Intro/C-Intro/main.c
@@ -8,13 +8,16 @@
 
 // getchar functions are included in stdio.h
 #include <stdio.h>
-
+#include "var.c"
 //getchar functions are included in stdio.h
+
 int main()
 {
   // \n is new line
     printf( "I am alive!  Beware.\n" );
 
+    int check1 = get_var();
+    printf("\nReturn Value: %d", check1);
     //Waits until the user hits enter to close the output window
     getchar();
 
--- C-Intro/C-Intro/var.c
+++ C-Intro/C-Intro/var.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 
-int 
+ int get_var ()
+ {
     int this_is_a_number;
 
     printf( "Please enter a number: " );
-    //Takes a number as input
+
     scanf( "%d", &this_is_a_number );
-    printf( "You entered %d", this_is_a_number );
+    printf( "You entered: %d", this_is_a_number );
     getchar();
-    return 0;
+    return 3;
+  }
